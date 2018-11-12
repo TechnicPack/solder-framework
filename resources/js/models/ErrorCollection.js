3db32829d07318d8fb4a19558bc3fab5d484deeb
Kyle Klaus <kklaus@indemnity83.com>
2018-11-12 15:23:55 -0800

    feat: api models track validation errors

---
+++ resources/js/models/ErrorCollection.js
@@ -0,0 +1,73 @@
+export default class ErrorCollection {
+    /**
+     * Construct a new ErrorCollection instance.
+     */
+    constructor() {
+        this.errors = {};
+    }
+
+    /**
+     * Determine if the collection has any errors.
+     */
+    hasErrors() {
+        return ! _.isEmpty(this.errors);
+    };
+
+
+    /**
+     * Determine if the collection has errors for a given field.
+     */
+    has(field) {
+        return _.indexOf(_.keys(this.errors), field) > -1;
+    };
+
+
+    /**
+     * Get all of the raw errors for the collection.
+     */
+    all() {
+        return this.errors;
+    };
+
+
+    /**
+     * Get all of the errors for the collection in a flat array.
+     */
+    flatten() {
+        return _.flatten(_.toArray(this.errors));
+    };
+
+
+    /**
+     * Get the first error message for a given field.
+     */
+    get(field) {
+        if (this.has(field)) {
+            return this.errors[field][0];
+        }
+    };
+
+
+    /**
+     * Set the raw errors for the collection.
+     */
+    set(errors) {
+        if (typeof errors === 'object') {
+            this.errors = errors;
+        } else {
+            this.errors = {'form': [__('Something went wrong. Please try again or contact customer support.')]};
+        }
+    };
+
+
+    /**
+     * Remove errors from the collection.
+     */
+    forget(field) {
+        if (typeof field === 'undefined') {
+            this.errors = {};
+        } else {
+            Vue.delete(this.errors, field);
+        }
+    };
+}
--- resources/js/models/Model.js
+++ resources/js/models/Model.js
@@ -1,6 +1,16 @@
 import { Model as BaseModel } from 'vue-api-query'
+import ErrorCollection from './ErrorCollection'
 
 export default class Model extends BaseModel {
+    /**
+     * Construct a new Model instance.
+     */
+    constructor(...atributtes) {
+        super(...atributtes);
+
+        this.busy = false;
+        this.errors = new ErrorCollection;
+    }
 
     // define a base url for a REST API
     baseURL () {
@@ -11,4 +21,36 @@ export default class Model extends BaseModel {
     request (config) {
         return this.$http.request(config)
     }
+
+    /**
+     * Start processing the model.
+     */
+    startProcessing() {
+        this.errors.forget();
+        this.busy = true;
+    };
+
+    /**
+     * Finish processing the model.
+     */
+    finishProcessing() {
+        this.busy = false;
+    };
+
+    /**
+     * Reset the errors and other state for the model.
+     */
+    resetStatus() {
+        this.errors.forget();
+        this.busy = false;
+    };
+
+
+    /**
+     * Set the errors on the model.
+     */
+    setErrors(errors) {
+        this.busy = false;
+        this.errors.set(errors);
+    };
 }
