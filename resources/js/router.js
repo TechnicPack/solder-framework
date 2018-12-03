195f2371f89ef33a4a015deb9776a8463d4c672b
Kyle Klaus <kklaus@indemnity83.com>
2018-12-02 21:22:26 -0800

    fix: put dashboard at the root of the app

--- resources/js/router.js
+++ resources/js/router.js
@@ -9,8 +9,7 @@ export default new Router({
     mode: 'history',
     base: '/app',
     routes: [
-        { path: '/', redirect: '/dashboard' },
-        { path: '/dashboard', name: 'dashboard', component: Dashboard },
+        { path: '/', name: 'dashboard', component: Dashboard },
         { path: '/modpack/:modpackId', name: 'modpack', component: Modpack, props: true },
         { path: '/modpack/:modpackId/build/:buildId', name: 'build', component: Build, props: true },
         { path: '/mod/:modId', name: 'mod', component: Mod, props: true },
